7fa677f281377e972fb9bb10edd0046b03aa2395
“luckyNB” <20150773@dbatu.ac.in>
2026-08-13 13:00:28 +0530

    [Laxman] - changed URL to login

--- src/test/java/com/ui/pages/MyAccountPage.java
+++ src/test/java/com/ui/pages/MyAccountPage.java
@@ -9,7 +9,7 @@ import com.utility.BrowserUtility;
 public final class MyAccountPage extends BrowserUtility {
 	private static final By USER_NAME_LOCATOR = By.xpath("//a[@title=\"View my customer account\"]/span");
 	private static final By SEARCH_TEXT_BOX_LOCATOR = By.id("search_query_top");
-	private static final By ADD_NEW_ADDRESS_LINK_LOCATOR = By.xpath("//a[@title=\"Add my first address\"]");
+	private static final By ADD_NEW_ADDRESS_LINK_LOCATOR = By.xpath("//span[text()='Add my first address']");
 	
 	public MyAccountPage(WebDriver driver) {
 		super(driver);
